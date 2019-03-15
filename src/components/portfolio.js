ff93a790bfac26da9c836e91ddffb2333b416bc2
Valeriy Sibikovskyi <combdn@gmail.com>
2019-03-15 12:03:20 +0200

    Extracted work-thumbnail component

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import WorkThumbnail from './work-thumbnail';
 import data from './data';
 import './portfolio.scss';
 
+// Function to import files
 function importAll(r) {
   let images = {};
   r.keys().forEach(item => {
@@ -10,37 +12,46 @@ function importAll(r) {
   return images;
 }
 
+// Import images
 const images = importAll(
   require.context('../assets/', true, /\.(png|jpe?g|svg)$/)
 );
 
+// Import videos
 const videos = importAll(require.context('../assets/', true, /\.(mp4|mov)$/));
 
 export default class Portfolio extends Component {
   render() {
     let works = [];
     for (const work of data) {
-      console.log(work.file);
-      if (work.type === 'image') {
-        works.push(
-          <div className={work.wrapperClass + ' wrapper'}>
-            <img alt="" className={work.class} src={images[work.file]} />
-          </div>
-        );
-      } else if (work.type === 'video') {
-        works.push(
-          <div className={work.wrapperClass + ' wrapper'}>
-            <video
-              autoPlay
-              playsInline
-              muted={true}
-              loop
-              className={work.class}
-              src={videos[work.file]}
-            />
-          </div>
-        );
-      }
+      works.push(
+        <WorkThumbnail
+          wrapperClass={work.wrapperClass}
+          type={work.type}
+          file={work.type === 'image' ? images[work.file] : videos[work.file]}
+          class={work.class}
+        />
+      );
+      // if (work.type === 'image') {
+      //   works.push(
+      //     <div className={work.wrapperClass + ' wrapper'}>
+      //       <img alt="" className={work.class} src={images[work.file]} />
+      //     </div>
+      //   );
+      // } else if (work.type === 'video') {
+      //   works.push(
+      //     <div className={work.wrapperClass + ' wrapper'}>
+      //       <video
+      //         autoPlay
+      //         playsInline
+      //         muted={true}
+      //         loop
+      //         className={work.class}
+      //         src={videos[work.file]}
+      //       />
+      //     </div>
+      //   );
+      // }
     }
     return <div className="grid">{works}</div>;
   }
---
+++ src/components/work-thumbnail.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './work-thumbnail.scss';
+
+export default class WorkThumbnail extends Component {
+  render() {
+    if (this.props.type === 'image') {
+      return (
+        <div className={this.props.wrapperClass + ' wrapper'}>
+          <img alt="" className={this.props.class} src={this.props.file} />
+        </div>
+      );
+    } else if (this.props.type === 'video') {
+      return (
+        <div className={this.props.wrapperClass + ' wrapper'}>
+          <video
+            autoPlay
+            playsInline
+            muted={true}
+            loop
+            className={this.props.class}
+            src={this.props.file}
+          />
+        </div>
+      );
+    }
+  }
+}
