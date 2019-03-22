35311b1f885594ff954108e28cb0d99338fff2aa
Valeriy Sibikovskyi <combdn@gmail.com>
2019-03-22 10:34:33 +0200

    Implemented filter handler to filter down
    It only works on single tag, no way to undo, no button changes, etc.

--- src/components/filter-button.js
+++ src/components/filter-button.js
@@ -2,10 +2,19 @@ import React, { Component } from 'react';
 import './filter-button.scss';
 
 export default class FilterButton extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    this.props.clickHandler(this.props.tag);
+  }
+
   render() {
     // TODO: Add counter. It should change as the gallery is being filtered.
     return (
-      <div role="button" className="filter-button">
+      <div role="button" className="filter-button" onClick={this.handleClick}>
         {this.props.tag}
       </div>
     );
--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
 import uuid from 'uuid/v4';
 import FilterButton from './filter-button';
 import WorkThumbnail from './work-thumbnail';
@@ -6,31 +7,48 @@ import data from './data';
 import './portfolio.scss';
 
 // Function to import files
-function importAll(r) {
-  let images = {};
-  r.keys().forEach(item => {
-    images[item.replace('./', '')] = r(item);
+function importAll(context) {
+  let files = {};
+  context.keys().forEach(item => {
+    files[item.replace('./', '')] = context(item);
   });
-  return images;
+  return files;
 }
 
 // Import images
-const images = importAll(
+const imageFiles = importAll(
   require.context('../assets/', true, /\.(png|jpe?g|svg)$/)
 );
 
 // Import videos
-const videos = importAll(require.context('../assets/', true, /\.(mp4|mov)$/));
+const videoFiles = importAll(
+  require.context('../assets/', true, /\.(mp4|mov)$/)
+);
 
 export default class Portfolio extends Component {
   constructor(props) {
     super(props);
-    this.filter = this.handleFilter.bind(this);
+    //debugger;
+    this.handleFilterClick = this.handleFilterClick.bind(this);
+    this.state = { dataToShow: data };
   }
 
-  handleFilter(e) {
+  handleFilterClick(tag) {
     // TODO: Implement filter handler
-    console.log('click');
+    console.log(tag);
+    console.log(this.state.dataToShow);
+
+    // let dataToFilter = _.clone(this.state.dataToShow);
+    // dataToFilter.filter(element =>
+    //   element.tags.includes(tag)
+    // );
+    // console.log(dataToFilter);
+
+    this.setState({
+      dataToShow: this.state.dataToShow.filter(element =>
+        element.tags.includes(tag)
+      )
+    });
   }
 
   render() {
@@ -38,13 +56,17 @@ export default class Portfolio extends Component {
     let tags = new Set();
     let filterButtons = [];
 
-    for (const work of data) {
+    for (const work of this.state.dataToShow) {
       works.push(
         <WorkThumbnail
           key={uuid()}
           wrapperClass={work.wrapperClass}
           type={work.type}
-          file={work.type === 'image' ? images[work.file] : videos[work.file]}
+          file={
+            work.type === 'image'
+              ? imageFiles[work.file]
+              : videoFiles[work.file]
+          }
           class={work.class}
         />
       );
@@ -56,7 +78,11 @@ export default class Portfolio extends Component {
 
     tags.forEach(tag =>
       filterButtons.push(
-        <FilterButton key={uuid()} tag={tag} onClick={this.handleFilter} />
+        <FilterButton
+          key={uuid()}
+          tag={tag}
+          clickHandler={this.handleFilterClick}
+        />
       )
     );
 
