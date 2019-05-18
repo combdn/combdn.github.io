c9824d1fe96dedb5afe0f789d9b6dc15a1529fa2
Valeriy Sibikovskyi <combdn@gmail.com>
2019-05-18 10:29:29 +0300

    Updates

---
+++ precache-manifest.9a66671a32fc2236557f6120ae191068.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98cf8498bbe4fbb52c3a23ff351741c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5c4c662966d46e29903",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "c999973865925a3b58eb",
+    "url": "/static/css/main.b115d781.chunk.css"
+  },
+  {
+    "revision": "c5c4c662966d46e29903",
+    "url": "/static/js/2.91d8904d.chunk.js"
+  },
+  {
+    "revision": "c999973865925a3b58eb",
+    "url": "/static/js/main.2aad3824.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4b9c3ea762112c1cc03f908699cfe830",
+    "url": "/static/media/3dLutCreator-Icon-no-bg.4b9c3ea7.png"
+  },
+  {
+    "revision": "fb905f815da1782629bbe97d59a026a8",
+    "url": "/static/media/3dLutCreator-Icon-test-preview-dribbble.fb905f81.png"
+  },
+  {
+    "revision": "60f0d417df4688dfe743dcbfa3419793",
+    "url": "/static/media/Agner-logo-big-perspective-2.60f0d417.png"
+  },
+  {
+    "revision": "278cf9c6b1c6f91eaf7e455cc105e274",
+    "url": "/static/media/Lighting_POC_animation_preview.278cf9c6.mov"
+  },
+  {
+    "revision": "88784501b87b773f51f89eba13b8e04e",
+    "url": "/static/media/Navarik-mockup-map-no-browser.88784501.png"
+  },
+  {
+    "revision": "97c4c970430739883d9fa02e7d988b10",
+    "url": "/static/media/Navarik-mockup-map-transparent.97c4c970.png"
+  },
+  {
+    "revision": "d90ecd636d1fac9397f435d49ba6ef9d",
+    "url": "/static/media/Navarik-mockup-map.d90ecd63.png"
+  },
+  {
+    "revision": "b69c9cfbd09a9bf7f009e35f722cbc5f",
+    "url": "/static/media/Navarik-mockup.b69c9cfb.png"
+  },
+  {
+    "revision": "8a7c247a9c8b356bd7750a7435f310aa",
+    "url": "/static/media/PW-concept.8a7c247a.mp4"
+  },
+  {
+    "revision": "5d7c73febe52823d274303c0be58d3b3",
+    "url": "/static/media/PW-deploy-icon.5d7c73fe.svg"
+  },
+  {
+    "revision": "c217e22034a0949998972a861ca7184a",
+    "url": "/static/media/PW-edit-task.c217e220.svg"
+  },
+  {
+    "revision": "47c9cb6dba94368090bb5adb7e4344df",
+    "url": "/static/media/PW-job-editing-cropped.47c9cb6d.png"
+  },
+  {
+    "revision": "2622cc3c9af3b3fb6ce4e4dd6eff5081",
+    "url": "/static/media/PW-job-editing.2622cc3c.png"
+  },
+  {
+    "revision": "08a6b58e779bf41f9abc99d593193c51",
+    "url": "/static/media/PW-navigator.08a6b58e.svg"
+  },
+  {
+    "revision": "e17d5cce3b68d58bd8764e4f5f478f38",
+    "url": "/static/media/PW-nodes-types.e17d5cce.png"
+  },
+  {
+    "revision": "b45e449676dc584cc408b6e072f23189",
+    "url": "/static/media/PW-properties.b45e4496.png"
+  },
+  {
+    "revision": "068c491fe45244c90524f5a8c32e8c8b",
+    "url": "/static/media/PW-real-case.068c491f.png"
+  },
+  {
+    "revision": "08dd3047a95c9a652d9aec8a352d7029",
+    "url": "/static/media/PW-signoff-cropped.08dd3047.png"
+  },
+  {
+    "revision": "a940224aa01eaeea9a43f3befce41d61",
+    "url": "/static/media/PW-signoff.a940224a.png"
+  },
+  {
+    "revision": "248d1290574828fd658b0dcf2d7e1875",
+    "url": "/static/media/QiVisor-HR.248d1290.png"
+  },
+  {
+    "revision": "a3501d276428a83027c15f89e4182489",
+    "url": "/static/media/Yawn-logo-blur-400-300.a3501d27.png"
+  },
+  {
+    "revision": "4cbb6a1d6495eb7455e31a04ced7b958",
+    "url": "/static/media/archmock-icon.4cbb6a1d.png"
+  },
+  {
+    "revision": "a9c3eb666cad3f229be1c4fffb10ba20",
+    "url": "/static/media/automation-assets-concept.a9c3eb66.mp4"
+  },
+  {
+    "revision": "fe115e832979f098f68e63793fe87aec",
+    "url": "/static/media/automation-fly-concept.fe115e83.mp4"
+  },
+  {
+    "revision": "d12c9c6ec7302b9481b2e408f84feb2d",
+    "url": "/static/media/automation-light.d12c9c6e.mp4"
+  },
+  {
+    "revision": "3d651faa25c30f25b5195cfe7f3bf8b5",
+    "url": "/static/media/cisco-latency-discovery.3d651faa.mp4"
+  },
+  {
+    "revision": "5c60790e8ee70fbe4821feb0dc89fbf0",
+    "url": "/static/media/cisco-risk-concept-v1.5c60790e.png"
+  },
+  {
+    "revision": "4c81323aadb866c0c60dc0e531445a17",
+    "url": "/static/media/daily-mail-concept.4c81323a.mp4"
+  },
+  {
+    "revision": "7f1c7e9121fde021a0dc941f79bf7112",
+    "url": "/static/media/diving-concept.7f1c7e91.png"
+  },
+  {
+    "revision": "c809ea99c95739cd2447eb106b4296d0",
+    "url": "/static/media/effect-doplera-cd-look.c809ea99.png"
+  },
+  {
+    "revision": "83f5a64d4a4f40a6fee3d290217932ad",
+    "url": "/static/media/houdini-actinia.83f5a64d.png"
+  },
+  {
+    "revision": "16a80346c8944bcbe9a3578270e8aee1",
+    "url": "/static/media/keepa-logo-dribbble.16a80346.png"
+  },
+  {
+    "revision": "f2e6489d36289a2edd88d6930ac32f49",
+    "url": "/static/media/patient-summary-cropped.f2e6489d.png"
+  },
+  {
+    "revision": "fe0aa9c0aa3bcd6b4a5662bcf826e492",
+    "url": "/static/media/sound-to-music.fe0aa9c0.png"
+  },
+  {
+    "revision": "afb1881ce48c2f3df6564b00c081cb4a",
+    "url": "/static/media/tatubalin-logo.afb1881c.svg"
+  },
+  {
+    "revision": "dabc2983348e96dbf84ad3445ecd9b54",
+    "url": "/static/media/twitter-icon.dabc2983.svg"
+  }
+]);
