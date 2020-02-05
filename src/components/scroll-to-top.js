f47ec3b42da6e230bb0a7cb3c988114fb237e1b2
Valery Sibikovsky <combdn@gmail.com>
2020-02-05 21:15:22 +0200

    Deselect the work on each navigation
    Because I scroll to the top every time, selected work makes no sense when I'm back to the home page.

--- src/components/scroll-to-top.js
+++ src/components/scroll-to-top.js
@@ -1,6 +1,33 @@
 import { useEffect } from 'react';
+import { connect } from 'react-redux';
 
-export default function ScrollToTop({ children, location }) {
-  useEffect(() => window.scrollTo(0, 0), [location.pathname]);
+import { turnOffWorkSelection } from '../features/gallery/gallerySlice';
+
+function ScrollToTop({ children, location, turnOffWorkSelection }) {
+  useEffect(() => {
+    /* Need to reset a work selection on any navigation.
+     Because I scroll to the top every time, selected work makes no sense when I'm back to the home page.
+    */
+
+    turnOffWorkSelection();
+
+    /* TODO: investigate managing scroll position in redux state. 
+     It might be convenient to do it from here:
+     
+     1. Post previouse location and scroll postion (it will remain the same).
+     2. Read position for the current location.
+     3. Tell window to scroll to that position.
+    */
+
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
   return children;
 }
+
+const mapDispatchToProps = {
+  turnOffWorkSelection
+};
+export default connect(
+  null,
+  mapDispatchToProps
+)(ScrollToTop);
